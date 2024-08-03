ae26539356a72bad2746552c29856a19eb547a31
PoojaB26 <pbhaumik26@gmail.com>
2024-08-03 20:54:36 +0530

    Update social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -57,7 +57,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card-docs.png',
     navbar: {
       title: 'FlutterFlow Docs',
       logo: {
